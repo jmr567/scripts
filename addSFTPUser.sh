a551e73e1e838c299be43ce8818ced911c878443
Joshua Roberts <joshua.m.roberts@gmail.com>
2013-03-28 09:56:27 +1100

    updated with html output and user checking

--- addSFTPUser.sh
+++ addSFTPUser.sh
@@ -9,19 +9,28 @@ if [ "$1" = "-h" ] ;
 	else 
 		if [ -z $1 ] ;
 		then echo "Usage: $0 {username} {days_to_expire}" && exit 1 ;
-		else echo "Adding user $USER"
+		#else echo "Adding user $USER"
+		else echo ""
 		fi
 fi
 
 if [[ $2 =~ ^[0-9]+$ ]] && [[ $2 -gt 0 ]]; 
-	then ACTUALEXPIRE=$2 && echo "Account will Expire after $ACTUALEXPIRE days" ;
-	else ACTUALEXPIRE=30 && echo "Account will Expire after the default of 30 days" ;
+	then ACTUALEXPIRE=$2 #&& echo "Account will Expire after $ACTUALEXPIRE days" ;
+	else ACTUALEXPIRE=30 #&& echo "Account will Expire after the default of 30 days" ;
 fi
+
+function checkUser {
+grep $USER /etc/passwd > /dev/null
+if [ $? = "0" ];
+	then echo "User already exists, no action taken" && exit 1 ;
+	else echo "" > /dev/null
+fi
+}
 
 function makeHomeDir {
 mkdir /home/$USER
 mkdir /home/$USER/incoming
-mkdir /home/$USER/outgoing
+#mkdir /home/$USER/outgoing
 }
 
 function addUser {
@@ -32,12 +41,12 @@ function fixPerms {
 chown root:root /home/$USER
 chmod 755 /home/$USER
 chmod 700 /home/$USER/incoming
-chmod 500 /home/$USER/outgoing
-chown $USER:sftp /home/$USER/incoming /home/$USER/outgoing
+#chmod 500 /home/$USER/outgoing
+chown $USER:sftp /home/$USER/incoming #/home/$USER/outgoing
 }
 
 function setPassword {
-echo $RANDOMPW | passwd $USER --stdin
+echo $RANDOMPW | passwd $USER --stdin > /dev/null 
 }
 
 function setExpire {
@@ -46,16 +55,19 @@ chage -E "$EXPIREDATE" $USER
 }
 
 function summary {
-echo ""
-echo "The User $USER with the password $RANDOMPW has SFTP access to ftp.services.ecetera.com.au for the next $ACTUALEXPIRE days"
-echo ""
-echo "The account will Expire on $EXPIREDATE"
-echo ""
-echo "To connect use # sftp $USER@ftp.services.ecetera.com.au"
-echo ""
+echo "<p>"
+echo "The User<b> $USER </b>with the password<b> $RANDOMPW </b>has SFTP access to<b> ftp.services.ecetera.com.au </b>for the next<b> $ACTUALEXPIRE </b>days"
+echo "</p>"
+echo "<p>"
+echo "The account will Expire on<b> $EXPIREDATE </b>"
+echo "</p>"
+echo "<p>"
+echo "To connect use <b> # sftp $USER@ftp.services.ecetera.com.au </b>"
+echo "</p>"
 }
 
 #Execute Functions
+checkUser
 makeHomeDir
 addUser
 fixPerms
